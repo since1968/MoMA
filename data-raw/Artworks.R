ba3b8a62c32be8e60ee549ec80e4096e67c813ad
since1968 <marc.garrett@gmail.com>
2015-09-24 12:30:10 -0400

    Cleaned up migration
    * removed old code
    * changed to relative paths

--- data-raw/Artworks.R
+++ data-raw/Artworks.R
@@ -1,13 +1,8 @@
 # load artworks from MoMA csv
 # TODO []: load from URL instead of local file for easier updates
 
-# raw <- read.csv("~/Source/since1968/MoMA/data-raw/Artworks.csv", header = TRUE, stringsAsFactors = FALSE)
-
-# artworks(raw) <- c("Title", "Artist", "ArtistBio", "Date", "Medium", "Dimensions", "CreditLine", "MoMANumber", "Classification", "Department", "DateAcquired", "CuratorApproved", "ObjectID", "URL")
-
-
 library(dplyr)
-csv <- "~/Source/since1968/MoMA/data-raw/Artworks.csv"
-target <- "~/Source/since1968/MoMA/data/artworks.Rdata"
+csv <- "data-raw/Artworks.csv"
+target <- "data/artworks.Rdata"
 artworks <- read.csv(csv, header = TRUE, stringsAsFactors = FALSE)
 save(artworks, file=target)
